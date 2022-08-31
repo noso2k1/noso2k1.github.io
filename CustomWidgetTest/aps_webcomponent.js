a17fc7847251066528dda1d32b69293104886d05
Gabriele N <gabriele.nosatti@gmail.com>
2022-08-31 16:15:33 +0200

    aps redrawing

--- CustomWidgetTest/aps_webcomponent.js
+++ CustomWidgetTest/aps_webcomponent.js
@@ -43,7 +43,7 @@
 
         //Fired when the widget is added to the html DOM of the page
         connectedCallback(){
-            this.redraw();
+            //this.redraw();
         }
 
          //Fired when the widget is removed from the html DOM of the page (e.g. by hide)
